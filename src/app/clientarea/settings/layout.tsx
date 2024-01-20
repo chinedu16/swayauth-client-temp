e1fbbb3c53d53b742f19378ecf9d8810cb3852be
John Fash <john@Johns-MacBook-Pro.local>
2024-01-20 13:05:41 +0100

    fixed isclient

--- src/app/clientarea/settings/layout.tsx
+++ src/app/clientarea/settings/layout.tsx
@@ -1,11 +1,7 @@
 "use server"
 import SettingNav from "@/components/clientarea/settings/settingNav";
 import Layout from "@/components/layout";
-import { CONST } from "@/lib/constant";
-import NavLink from "@/lib/navLink";
 import { scope } from "@/store/slice/account";
-import { jwtDecode } from "jwt-decode";
-import { cookies } from "next/headers";
 
 interface Decoded {
   permissions: ('read' | 'write' | 'delete')[]
@@ -13,16 +9,6 @@ interface Decoded {
   scope: scope[]
 }
 
-const isAccess = (access?: 'level_2' | 'level_3') => {
-  try {
-    const tk = cookies().get(CONST.ACCESS_TOKEN)?.value as never ?? ''
-    return jwtDecode<Decoded>(tk).access == access
-  } catch (error: any) {
-    return false
-  }
-}
-
-const isSuperAdmin = isAccess('level_3')
 
 const RootLayout = ({
   children,
@@ -33,7 +19,7 @@ const RootLayout = ({
   return <Layout>
     <div className="py-3 md:py-6 px-4 md:px-8">
       <h3 className="text-2xl font-bold">Settings</h3>
-      <SettingNav isSuperAdmin={isSuperAdmin} />
+      <SettingNav  />
       <div className="mt-8 w-full">
         {children}
       </div>
--- src/components/clientarea/settings/settingNav.tsx
+++ src/components/clientarea/settings/settingNav.tsx
@@ -1,14 +1,23 @@
 "use client"
-import NavLink from "@/lib/navLink";
 
-const SettingNav = ({ isSuperAdmin }: { isSuperAdmin: boolean }) => {
+import NavLink from "@/lib/navLink";
+import { isAccess } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+const isSuperAdmin = isAccess('level_3')
+
+const SettingNav = () => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true)
+  }, []);
   return <div className="mt-8 flex text-center sticky top-[3.9rem] bg-slate-100 z-20">
     <NavLink href='/clientarea/settings'
       activeClass='border-blue-700 font-bold'
       className={`inline-block ${isSuperAdmin ? 'w-4/12' : 'w-6/12'} p-2 border-b-2 transition-all`}
     >Account</NavLink>
     {
-      isSuperAdmin ?
+      isSuperAdmin && isClient ?
         <NavLink href='/clientarea/settings/wallet'
           activeClass='border-blue-700 font-bold'
           className="inline-block w-4/12 p-2 border-b-2 transition-all"
