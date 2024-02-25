0ddda1f14f02f68c5d8a4b0c61266f04a8461ddc
johnsonfash <fashanutosin7@gmail.com>
2024-02-25 09:29:13 +0100

    add prod

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -2,24 +2,21 @@
 import { CONST } from "@/lib/constant";
 import { useEffect } from "react";
 
-const testUrl = `${CONST.BASE_URL}/auth/google?client_id=65da91b31c1f6515540001e1`
-
 const CheckPage = () => {
 
   useEffect(() => {
-    window.swayauthInitialize(testUrl, 'GOOGLE-BUTTON', (data) => {
-      console.log(data);
-    })
+    try {
+      const url = `${CONST.BASE_URL}/auth/google?client_id=65da91b31c1f6515540001e1`
+      window?.swayauthInitialize(url, 'GOOGLE-BUTTON', (data) => {
+        console.log(data);
+      })
+    } catch (error: any) { }
   }, []);
-
-  const handleLogin = () => {
-
-  }
 
   return <main className="h-screen text-center w-screen flex items-center justify-center">
     <div>
       <div className="text-2xl mb-3">Test Login</div>
-      <button id="GOOGLE-BUTTON" onClick={handleLogin} className="py-2 px-4 bg-blue-700 text-white rounded-md">Google Login</button>
+      <button id="GOOGLE-BUTTON" className="py-2 px-4 bg-blue-700 text-white rounded-md">Google Login</button>
     </div>
   </main>;
 };
